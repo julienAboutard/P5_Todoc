81edb2a4c6f678003c9d460e8b2d99a5b841f105
Julien <julien.aboutard@gmail.com>
2023-11-21 23:23:36 +0100

    Dao

---
+++ app/src/main/java/com/example/todoc/data/TaskRepository.java
@@ -0,0 +1,44 @@
+package com.example.todoc.data;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
+import com.example.todoc.data.dao.ProjectDao;
+import com.example.todoc.data.dao.TaskDao;
+import com.example.todoc.data.entity.Project;
+import com.example.todoc.data.entity.ProjectWithTasks;
+import com.example.todoc.data.entity.Task;
+
+import java.util.List;
+
+public class TaskRepository {
+
+    @NonNull
+    private TaskDao taskDao;
+
+    @NonNull
+    private ProjectDao projectDao;
+
+    public TaskRepository(
+        @NonNull TaskDao taskDao,
+        @NonNull ProjectDao projectDao
+    ) {
+        this.taskDao = taskDao;
+        this.projectDao = projectDao;
+    }
+    public LiveData<List<Project>> getAllProject() {
+        return projectDao.getAll();
+    }
+
+    public LiveData<List<ProjectWithTasks>> getAllProjectWthTasks() {
+        return taskDao.getAllProjectWithTasks();
+    }
+
+    public void addTask(Task task) {
+        taskDao.insert(task);
+    }
+
+    public void deleteTask(int taskId) {
+        taskDao.delete(taskId);
+    }
+}
--- app/src/main/java/com/example/todoc/data/TodocDatabase.java
+++ app/src/main/java/com/example/todoc/data/TodocDatabase.java
@@ -1,26 +1,84 @@
 package com.example.todoc.data;
 
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
+import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.todoc.R;
 import com.example.todoc.data.dao.ProjectDao;
 import com.example.todoc.data.dao.TaskDao;
 import com.example.todoc.data.entity.Project;
 import com.example.todoc.data.entity.Task;
 
+import java.util.concurrent.Executor;
+
 @Database(
     entities = {Task.class, Project.class},
     version = 1
 )
-public class TodocDatabase extends RoomDatabase {
+public abstract class TodocDatabase extends RoomDatabase {
 
     private static final String DATABASE_NAME = "todoc_database";
 
-    private static final TodocDatabase instance;
+    private static TodocDatabase instance;
 
-    public abstract TaskDao taskDao();
+    public abstract TaskDao getTaskDao();
 
-    public abstract ProjectDao projectDao;
+    public abstract ProjectDao getProjectDao();
+
+    public static synchronized TodocDatabase getInstance(
+        @NonNull Context context,
+        @NonNull Executor executor
+    ) {
+        if (instance == null) {
+            instance = createDB(context, executor);
+        }
+        return instance;
+    }
+
+    public static synchronized TodocDatabase createDB(
+        @NonNull Context context,
+        @NonNull Executor executor
+    ) {
+        Builder<TodocDatabase> databaseBuilder = Room.databaseBuilder(
+            context.getApplicationContext(),
+            TodocDatabase.class,
+            DATABASE_NAME
+        );
+
+        databaseBuilder.addCallback(new Callback() {
+            @Override
+            public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        ProjectDao projectDao = TodocDatabase.instance.getProjectDao();
+
+                        projectDao.insert(
+                            new Project(
+                                context.getString(R.string.tartampionp),
+                                ResourcesCompat.getColor(context.getResources(), R.color.tartampionc, null)
+                            )
+                        );
+                    }
+                });
+            }
+        });
+    }
+    private static RoomDatabase.Callback roomCallBack = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+        }
+    };
+
+
 
 
 }
